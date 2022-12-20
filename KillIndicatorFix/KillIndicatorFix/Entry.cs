c4e3070d4f8b5ba18e7f29ea8b71a4093ed978ab
randomuserhi <magicalworld.nano@gmail.com>
2022-12-20 00:46:31 +0000

    Updated version

--- KillIndicatorFix/KillIndicatorFix/Entry.cs
+++ KillIndicatorFix/KillIndicatorFix/Entry.cs
@@ -14,7 +14,7 @@ namespace KillIndicatorFix
     {
         public const string GUID = "randomuserhi.KillIndicatorFix";
         public const string Name = "KillIndicatorFix";
-        public const string Version = "0.0.1";
+        public const string Version = "0.0.3";
     }
 
     [BepInPlugin(Module.GUID, Module.Name, Module.Version)]
